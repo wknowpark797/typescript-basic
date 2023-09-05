84253f09dfc69a83aa669837e7a9a44c40c63803
wknowpark797 <wknowpark797@gmail.com>
2023-09-05 10:45:08 +0900

    기본 자료형, 배열 자료형 타입 지정

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-const plus = (n1: number, n2: number) => {
-	return n1 + n2;
-};
+// 기본 타입
+let test: string = 'hello';
+let num: number = 0;
+let isFemale: boolean = true;
 
-const result = plus(2, 3);
-console.log(result);
+// 참조형 타입
+let like: string[] = ['game', 'music', 'movie'];
+let dislike: Array<string> = ['study', 'homework'];
+let odd: number[] = [1, 3, 5];
+let even: Array<number> = [2, 4, 6];
+let test2: boolean[] = [true, false, true];
+let test3: Array<boolean> = [true, false, true];
 
-/*
-  ts파일을 일반 js파일로 컴파일해야 한다.
-  - tsc -w 변환활파일명.ts
-  - tsc -w 변환할파일명.ts --target es6
-*/
+// tuple: 배열에 들어가는 자료형이 다를 때 배열의 순서마다 타입 지정
+let arr1: [number, string] = [3, '3'];
