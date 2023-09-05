750b70335013a420d742c9b4b377759a7622684f
wknowpark797 <wknowpark797@gmail.com>
2023-09-05 10:23:07 +0900

    typescript basic init

---
+++ main.ts
@@ -0,0 +1,12 @@
+const plus = (n1: number, n2: number) => {
+	return n1 + n2;
+};
+
+const result = plus(2, 3);
+console.log(result);
+
+/*
+  ts파일을 일반 js파일로 컴파일해야 한다.
+  - tsc -w 변환활파일명.ts
+  - tsc -w 변환할파일명.ts --target es6
+*/
